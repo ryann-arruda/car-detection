2fd14d2290587633909395153a461fe0ca5d54f7
Ryann <ryann.arruda_159@hotmail.com>
2024-10-13 21:09:47 -0300

    Changing the number or iterations

--- train.py
+++ train.py
@@ -18,9 +18,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Learning parameters
 checkpoint = None  # path to model checkpoint, None if none
 batch_size = 8  # batch size
-iterations = 120000  # number of iterations to train
+iterations = 197  # number of iterations to train
 workers = 4  # number of workers for loading data in the DataLoader
-print_freq = 200  # print training status every __ batches
+print_freq = 5  # print training status every __ batches
 lr = 1e-3  # learning rate
 decay_lr_at = [80000, 100000]  # decay learning rate after these many iterations
 decay_lr_to = 0.1  # decay learning rate to this fraction of the existing learning rate
